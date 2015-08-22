c747f44e9e0262462bd331a8d5d4aa8286cfc32b
Emba Moussa <emoussa@intellicentrics.com>
2015-08-22 13:49:23 +0100

    an Event may have more than one link

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,5 @@ class Event < ActiveRecord::Base
   attr_accessible :artists, :contact, :description, :door_time, :end_time, :hashtag, :keywords, :last_entry_time, :organiser_name, :package, :start_time, :summary, :timestamp, :title
   validates :title, :start_time, presence: true
   has_many :images
+  has_many :links
 end
