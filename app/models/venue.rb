b02f42441c6d6084e72b1f63ea483f5e199ecaa8
Emba Moussa <emoussa@intellicentrics.com>
2015-08-22 13:51:06 +0100

    an Event is run in one place/venue

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,4 +1,6 @@
 class Venue < ActiveRecord::Base
   attr_accessible :address_1, :address_2, :country, :latitude, :longitude, :name, :postcode, :town
   validates :name, presence: true
+
+  has_many :events
 end
