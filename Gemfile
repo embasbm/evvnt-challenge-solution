1a885d894456eef7954c38b6257880c80bb5bf88
Emba Moussa <emoussa@intellicentrics.com>
2015-08-23 16:08:05 +0100

    testing external http request using stubs

--- Gemfile
+++ Gemfile
@@ -10,6 +10,10 @@ group :assets do
   gem 'uglifier', '>= 1.0.3'
 end
 
+group :test do
+  gem 'capybara'
+  gem 'webmock'
+end
 group :development, :test do
   gem 'rspec-rails'
   gem 'shoulda-matchers'
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'shoulda/matchers'
+require 'capybara/rspec'
 RSpec.configure do |config|
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
   config.use_transactional_fixtures = true
