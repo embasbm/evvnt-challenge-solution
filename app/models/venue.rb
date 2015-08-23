c91eb22be807c62e3cb13896c614e8df9769f37a
Emba Moussa <emoussa@intellicentrics.com>
2015-08-23 16:10:29 +0100

    save imported data in Venue model

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,6 +1,14 @@
 class Venue < ActiveRecord::Base
-  attr_accessible :address_1, :address_2, :country, :latitude, :longitude, :name, :postcode, :town
+  attr_accessible :id, :address_1, :address_2, :country, :latitude, :longitude, :name, :postcode, :town
   validates :name, presence: true
 
   has_many :events
+
+  def self.save_in_local_db(venue)
+    unless self.exists?(venue[:id])
+      existing_venue = self.new
+      existing_venue.assign_attributes(venue)
+      existing_venue.save
+    end
+  end
 end
