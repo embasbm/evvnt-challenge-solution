90fb53e23f88ac0f056562ae406ea7e87a4dd012
Emba Moussa <emoussa@intellicentrics.com>
2015-08-23 16:09:02 +0100

    save imported data in Image model

--- spec/models/image_spec.rb
+++ spec/models/image_spec.rb
@@ -2,5 +2,6 @@ require 'rails_helper'
 
 describe Image do
   it { should validate_presence_of(:image_url) }
+  it { should validate_uniqueness_of(:image_url).scoped_to(:event_id) }
   it { should belong_to(:event)}
 end
