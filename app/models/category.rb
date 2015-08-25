499342c9ef14741426a9955bf0171f21cc0a424c
Emba Moussa <emoussa@intellicentrics.com>
2015-08-25 16:27:57 +0100

    refactor insert data into category model

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,4 +3,12 @@ class Category < ActiveRecord::Base
   validates :name, presence: true
 
   has_many :events
+
+  def self.find_or_create_from_list(categories_set)
+    categories_set.each do |category|
+      unless self.exists?(category.id)
+        Category.create(category.attributes)
+      end
+    end
+  end
 end
