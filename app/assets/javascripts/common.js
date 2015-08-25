0e3b980d556e711b8843a371a653d47d1d963be9
Emba Moussa <emoussa@intellicentrics.com>
2015-08-25 13:45:20 +0100

    include actual ajax call

--- app/assets/javascripts/common.js
+++ app/assets/javascripts/common.js
@@ -1,3 +1,8 @@
 $(document).ready(function(){
   $('input.datepicker').datepicker();
+
+  $('#events_filter').on("ajax:success", function(e, data, status, xhr){
+    // console.log(e,'data: ', data, status, xhr)
+    $("#main_content").html(data);
+  });
 });
