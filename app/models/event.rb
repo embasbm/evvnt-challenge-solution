0824a4989100b424264a5999adb3d959034e9d4c
Emba Moussa <emoussa@intellicentrics.com>
2015-08-25 13:48:06 +0100

    move logic to model instead of controller, refactor event scaffold

--- app/models/event.rb
+++ app/models/event.rb
@@ -8,14 +8,31 @@ class Event < ActiveRecord::Base
   has_many :links
   has_many :prices
 
-
-  def self.save_in_local_db(attributes, venue_id)
-    venue_attribute = ActiveSupport::HashWithIndifferentAccess.new
-    venue_attribute[:venue_id] = venue_id
-    attributes.except!(:image_url, :image_urls, :links, :prices, :venue)
-    attributes.merge!(venue_attribute)
-    new_event = Event.new
-    new_event.assign_attributes(attributes)
-    new_event.save
+  def self.by_keyword(query) 
+    where("title LIKE ? OR summary LIKE ? OR description LIKE ? OR keywords LIKE ?", "%#{query}%", "%#{query}%", "%#{query}%", "%#{query}%")
   end
+
+  def self.find_or_create_from_attributes(event)
+    unless Event.exists?(event.id)
+      venue = Venue.find_or_create_by_id(event.venue.attributes)
+      event_attributes = event.attributes.except(:links, :image_url, :image_urls, :prices, :venue)
+          .merge(venue_id: venue.id)
+      new_event = Event.create(event_attributes)
+
+      event.image_urls.push(event.image_url).uniq.each do |image_url|
+        new_event.images.find_or_create_by_image_url(image_url)
+      end
+
+      event.prices.attributes.keys.each do |price_category|
+        new_event.prices.create(criteria: price_category, value: event.prices.attributes[price_category])
+      end
+      
+      event.links.attributes.keys.each do |link_name|
+        new_event.links.create(name: link_name, url: event.link.attributes[link_name])
+      end
+
+    end
+  end
+
+
 end
