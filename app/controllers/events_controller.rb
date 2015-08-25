93488fe26cfd770704a7ab8c90fa09f8f0bb13a6
Emba Moussa <emoussa@intellicentrics.com>
2015-08-25 13:47:11 +0100

    refactor events controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,41 +1,13 @@
 class EventsController < ApplicationController
   include AbstractController::Callbacks
 
-  # after_filter :fetch_categories, :fetch_events
-
   def index
-    @categories = Category.parent_categories
+
     @events = Event.order(:start_time).page(params[:page]).per(5)
+    @events = @events.by_keyword(params[:keyword]) if params[:keyword].present?
+    @events = @events.where('start_time > ?', Time.parse(params[:date_from])) if params[:date_from].present?
+    @events = @events.where('start_time < ?', Time.parse(params[:date_to])) if params[:date_to].present?
+    @events = @events.where(venue_id: params[:venue_id]) if params[:venue_id].present?
   end
 
-  private
-
-  def fetch_categories
-    Category.fetch_all_categories
-  end
-
-  def fetch_events
-    begin
-      event_list = Api::Event.all
-    rescue ActiveResource::ResourceNotFound => e
-      Rails.logger.debug "#{e}"
-    rescue ActiveResource::ResourceConflict, ActiveResource::ResourceInvalid => e
-      Rails.logger.debug "#{e}"
-    rescue Timeout::Error => e
-      Rails.logger.debug "#{e}"
-    end
-    save_in_local_db(event_list) if event_list.present?
-  end
-
-  def save_in_local_db(event_list)
-    event_list.each do |event|
-      unless Event.exists?(event.id)
-        Image.save_in_local_db(event.id, event.image_url, event.image_urls)
-        Link.save_in_local_db(event.id, event.links.attributes)
-        Price.save_in_local_db(event.id, event.prices.attributes)
-        Venue.save_in_local_db(event.venue.attributes)
-        Event.save_in_local_db(event.attributes, event.venue.attributes[:id])
-      end
-    end
-  end
 end
