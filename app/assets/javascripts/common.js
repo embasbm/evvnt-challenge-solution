30786d49ad32be64ce6b7bc753eb3d89701e883f
Emba Moussa <emoussa@intellicentrics.com>
2015-08-25 14:11:06 +0100

    include js source

--- app/assets/javascripts/common.js
+++ app/assets/javascripts/common.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
   $('input.datepicker').datepicker();
 
+  //  => source: http://edgeguides.rubyonrails.org/working_with_javascript_in_rails.html
   $('#events_filter').on("ajax:success", function(e, data, status, xhr){
     // console.log(e,'data: ', data, status, xhr)
     $("#main_content").html(data);
