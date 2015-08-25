1eeb56a70ff0c55b7edcc0bc18652243e8c09c13
Emba Moussa <emoussa@intellicentrics.com>
2015-08-25 18:33:17 +0100

    refactor rake task to import data

--- app/models/event.rb
+++ app/models/event.rb
@@ -12,27 +12,27 @@ class Event < ActiveRecord::Base
     where("title LIKE ? OR summary LIKE ? OR description LIKE ? OR keywords LIKE ?", "%#{query}%", "%#{query}%", "%#{query}%", "%#{query}%")
   end
 
-  def self.find_or_create_from_attributes(event)
-    unless Event.exists?(event.id)
-      venue = Venue.find_or_create_by_id(event.venue.attributes)
-      event_attributes = event.attributes.except(:links, :image_url, :image_urls, :prices, :venue)
-          .merge(venue_id: venue.id)
-      new_event = Event.create(event_attributes)
+  def self.find_or_create_from_attributes(events)
+    events.each do |event|
+      unless Event.exists?(event.id)
+        venue = Venue.find_or_create_by_id(event.venue.attributes)
+        event_attributes = event.attributes.except(:links, :image_url, :image_urls, :prices, :venue)
+            .merge(venue_id: venue.id)
+        new_event = Event.create(event_attributes)
 
-      event.image_urls.push(event.image_url).uniq.each do |image_url|
-        new_event.images.find_or_create_by_image_url(image_url)
-      end
+        event.image_urls.push(event.image_url).uniq.each do |image_url|
+          new_event.images.find_or_create_by_image_url(image_url)
+        end
 
-      event.prices.attributes.keys.each do |price_category|
-        new_event.prices.create(category: price_category, value: event.prices.attributes[price_category])
-      end
-      
-      event.links.attributes.keys.each do |link_name|
-        new_event.links.create(name: link_name, url: event.link.attributes[link_name])
-      end
+        event.prices.attributes.keys.each do |price_category|
+          new_event.prices.create(category: price_category, value: event.prices.attributes[price_category])
+        end
 
+        event.links.attributes.keys.each do |link_name|
+          new_event.links.create(name: link_name, url: event.links.attributes[link_name])
+        end
+      end
     end
   end
-
 
 end
--- lib/tasks/fetch_api_data.rake
+++ lib/tasks/fetch_api_data.rake
@@ -2,21 +2,23 @@ namespace :import do
   desc 'Import Categories and Events from Sandbox API'
   task :categories_and_events => :environment do
 
+    events = Array.new(0)
+    categories = Array.new(0)
+
     begin
       categories = Api::Category.all
-      Category.find_or_create_from_list(categories)
     rescue => e
       Rails.logger.error "Error fetching Categories!!!\n #{e.inspect}"
     end
+    Category.find_or_create_from_list(categories)
 
     begin
       events = Api::Event.all
-      events.each do |event|
-        Event.find_or_create_from_attributes(event)
-      end
     rescue => e
       Rails.logger.error "Error fetching events!!!\n #{e.inspect}"
     end
+    Event.find_or_create_from_attributes(events)
+
   end
 end
 
