3f6c91c39a0531411e31be651e510d02e3f9bedd
Emba Moussa <emoussa@intellicentrics.com>
2015-08-25 13:46:11 +0100

    manage ajax call trough rails

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,26 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :find_venues, :get_edge_dates, unless: :ajax_request?
+
+  layout :set_layout
+
+  protected
+
+  def set_layout
+    ajax_request? ? false : 'application' # layout is false for ajax requests
+  end
+
+  def ajax_request?
+    request.xhr?
+  end
+
+  def find_venues
+    @venues = Venue.all
+  end
+
+  def get_edge_dates
+    @min_date = Event.minimum(:start_time)
+    @max_date = Event.maximum(:start_time)
+  end
 end
